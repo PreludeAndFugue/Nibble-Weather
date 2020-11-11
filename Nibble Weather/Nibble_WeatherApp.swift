d8a18dab53482b4d24713912327c5dfc71dd749a
Gary Kerr <garykerrwork@gmail.com>
2020-11-11 18:31:38 +0000

    Initial Commit

---
+++ Nibble Weather/Nibble_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nibble_WeatherApp.swift
+//  Nibble Weather
+//
+//  Created by gary on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Nibble_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
